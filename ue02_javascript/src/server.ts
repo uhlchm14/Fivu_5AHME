49bc06091eaa376eb5b254973f0cd5fbd77905ea
uhlchm14 <32091901+uhlchm14@users.noreply.github.com>
2018-11-30 11:38:49 +0100

    Add files via upload

--- ue02_javascript/src/server.ts
+++ ue02_javascript/src/server.ts
@@ -1,24 +1,34 @@
 
 // Node.js Modul
+// import * as http from 'http';
 import * as path from 'path';
-import * as express from 'express';
-var bodyParser = require('body-parser');
+import * as bodyParser from 'body-parser';
 
+// Externes Modul (via npm instaliieren)
+import * as express from 'express';
 
 export class Server {
 
     private _port: number;
     private _server: express.Express;
 
-     constructor (port: number) {
+    public constructor (port: number) {
         const assetsPath = path.join(__dirname, '..', 'assets');
         this._port = port;
         this._server = express();
-        this._server.use('/public', express.static(assetsPath));
-        this._server.use(bodyParser);
-        this._server.post('login.html', (req, res, next) => this.handlePostLogin(req, res, next));
-        this._server.get('/liste', (req, res, next) => this.handleGetListe(req, res, next));
-        this._server.get('image.png', (req, res, next) => this.handleSendImage(res));
+        this._server.set('views', path.join(__dirname, 'views'));
+        const engine = this._server.set('view engine', 'pug');
+        engine.locals.pretty = true;
+
+        this._server.use('/', express.static(assetsPath));
+        this._server.use(bodyParser.json());
+        this._server.use(bodyParser.urlencoded());
+        this._server.post('/login.html',
+            (req, res, next) => this.handlePostLogin(req, res, next)
+        );
+        this._server.get('/liste',
+          (req, res, next) => this.handleGetListe(req, res, next)
+        );
     }
 
     public start () {
@@ -30,17 +40,23 @@ export class Server {
         return this._port;
     }
 
-    private handlePostLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
-        debugger();
+    private handlePostLogin (req: express.Request, res: express.Response,
+        next: express.NextFunction) 
+    {
+        if (req.body.email === 'test@test.at' && req.body.password === 'geheim'){
+            res.render('welcome.pug', {andrede: 'Herr', name: 'Rossi'});
+        } else {
+            res.status(404).send('404 ok cool');
+        }
         next();
     }
 
-    private handleGetListe(req: express.Request, res: express.Response, next: express.NextFunction) {
-        const filePath = path.join(__dirname, '../assets', 'liste.html');
-        res.sendFile(filePath);
-    }
-    private handleSendImage(res: express.Response) {
-        const filePath = path.join(__dirname, '../assets', 'image.png');
+
+    private handleGetListe (req: express.Request, res: express.Response,
+                            next: express.NextFunction) {
+        // res.send('Guten Morgen, Herr Muri');
+        const filePath = path.join(__dirname, '..', 'assets', 'liste.html');
+        console.log(filePath);
         res.sendFile(filePath);
     }
 }
