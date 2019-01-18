d9c56f06e901381892023017ba8f1ceaa83acd45
uhlchm14 <32091901+uhlchm14@users.noreply.github.com>
2019-01-18 11:53:54 +0100

    Add files via upload

---
+++ ue03_Angular/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'ue03Angular';
+  public clock = '??:??:??';
+  public items = [
+    { myClass: 'red', text: '1' },
+    { myClass: 'red', text: '2' },
+    { myClass: 'green', text: '3' }
+   ];
+  public myClass = 'green';
+
+  public constructor () {
+      this.clock = new Date().toLocaleTimeString();
+      // setTimeout( () => {
+
+      // }, 1000);
+      setInterval( () => this.handleTimer(), 1000);
+  }
+
+  private handleTimer () {
+    this.clock = new Date().toLocaleTimeString();
+    // this.items.push('' + (+this.items[this.items.length - 1] + 1));
+  }
+
+
+
+}
